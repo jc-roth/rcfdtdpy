b05ad9630985feb3b408f61e1e645956bf556f5b
Jack-SLAC <jroth@dhcp-swlan-public-84-142.slac.stanford.edu>
2018-07-05 16:52:10 -0700

    Added test code to the example.py script

--- src/example.py
+++ src/example.py
@@ -13,30 +13,34 @@ def prep_sim():
 
 if __name__ == '__main__':
     # Prepare constants
-    vacuum_permittivity = 4*np.pi*(10.0**(-7))
-    infinity_permittivity = 0
-    vacuum_permability = 8.854187817 * (10**(-12))
-    delta_z = 3e4
-    delta_t = 3e-4
+    vacuum_permittivity = 8.854187817e-12
+    vacuum_permability = (4 * np.pi)*1e-7
+    infinity_permittivity = 1
+    initial_susceptability = 0
+    delta_z = 3e-6
+    delta_t = 1
 
-    dim_n = 10000
-    dim_i = 2000
+    dim_n = 10
+    dim_i = 500
 
     # Prepare current field
     cfield = Field(dim_n, dim_i)
-    cfield[200] = 0.500
+    cfield.set_time_index(2)
+    cfield[250] = 1
 
     # Prepare and perform simulation
-    s = Sim(vacuum_permittivity, infinity_permittivity, vacuum_permability, delta_t, delta_z, dim_n, dim_i, cfield, 0, 0)
-    #s.simulate()
+    s = Sim(vacuum_permittivity, infinity_permittivity, vacuum_permability, delta_t, delta_z, dim_n, dim_i, cfield, 0, initial_susceptability)
+    s.simulate()
 
     # Export simulation result
-    #arr = s.get_efield().export()
-    arr = np.load('sim.npy')
+    e = s.get_efield().export()
+    h = s.get_hfield().export()
+    #arr = np.load('sim.npy')
 
     # Save the result
     #np.save('sim.npy', arr)
 
     # Visualize result
-    print(np.shape(arr))
-    vis.contor_plot(arr[0:50,100:300])
+    print(np.shape(e))
+    print(np.shape(h))
+    #vis.contor_plot(e, h)
