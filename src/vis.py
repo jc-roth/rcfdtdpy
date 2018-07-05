dd053951b3bc2bef6b0018ae01c2addb2f7645ee
Jack-SLAC <jroth@dhcp-swlan-public-84-142.slac.stanford.edu>
2018-07-05 16:51:08 -0700

    Updated visualization code

--- src/vis.py
+++ src/vis.py
@@ -10,35 +10,49 @@ from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
 import numpy as np
 
-def contor_plot(arr):
+def contor_plot(e, h):
     # make these smaller to increase the resolution
     # delta_z, delta_t
     
     # generate 2 2d grids for the x & y bounds
-    dim_t, dim_z = np.shape(arr)
+    dim_t, dim_z = np.shape(e)
     t, z = np.mgrid[slice(0, dim_t, 1),
                     slice(0, dim_z, 1)]
 
-    print(np.shape(t))
-    print(np.shape(z))
+    ie = np.real(e)
+    ih = np.real(h)
 
-    i = np.real(arr)
+    fig, axes = plt.subplots(nrows=2, sharex=True)
 
-    # x and y are bounds, so z should be the value *inside* those bounds.
-    # Therefore, remove the last value from the z array.
-    i = i[:-1, :-1]
-    levels = MaxNLocator(nbins=5).tick_values(i.min(), i.max())
+    # t and z are bounds, so i should be the value *inside* those bounds.
+    # Therefore, remove the last value from the i array.
+    ie = ie[:-1, :-1]
+    levelse = MaxNLocator(nbins=500).tick_values(ie.min(), ie.max())
 
 
     # pick the desired colormap, sensible levels, and define a normalization
     # instance which takes data values and translates those into levels.
-    cmap = plt.get_cmap('PiYG')
-    norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+    cmape = plt.get_cmap('PiYG')
+    norme = BoundaryNorm(levelse, ncolors=cmape.N, clip=True)
 
-    im = plt.pcolormesh(z, t, i, cmap=cmap, norm=norm)
-    plt.colorbar(im)
-    plt.title('pcolormesh with levels')
-    plt.xlabel('space [i]')
-    plt.ylabel('time [n]')
+    ime = axes[0].pcolormesh(z, t, ie, cmap=cmape, norm=norme)
+    fig.colorbar(ime, ax=axes[0])
+    axes[0].set_ylabel('E\ntime [n]')
+
+    # t and z are bounds, so i should be the value *inside* those bounds.
+    # Therefore, remove the last value from the i array.
+    ih = ih[:-1, :-1]
+    levelsh = MaxNLocator(nbins=500).tick_values(ih.min(), ih.max())
+
+
+    # pick the desired colormap, sensible levels, and define a normalization
+    # instance which takes data values and translates those into levels.
+    cmaph = plt.get_cmap('PiYG')
+    normh = BoundaryNorm(levelsh, ncolors=cmaph.N, clip=True)
+
+    imh = axes[1].pcolormesh(z, t, ih, cmap=cmaph, norm=normh)
+    fig.colorbar(imh, ax=axes[1])
+    axes[1].set_xlabel('space [i]')
+    axes[1].set_ylabel('H\ntime [n]')
 
     plt.show()
