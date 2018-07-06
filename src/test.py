e7a3650be73e993b126defd337b6fa61ea681eb5
Jack-SLAC <jroth@dhcp-swlan-public-84-142.slac.stanford.edu>
2018-07-06 15:28:43 -0700

    Added current code to the test module

--- src/test.py
+++ src/test.py
@@ -2,12 +2,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    hfield = np.zeros(102)
-    efield = np.zeros(102)
+
+    time_dim = 50
+    space_dim = 102
+
+    hfield = np.zeros(space_dim)
+    efield = np.zeros(space_dim)
+
+    current = np.zeros((time_dim, space_dim))
+
+    for n in range(time_dim):
+        current[n, 49] = np.sin(2*np.pi*(n/15))
 
     xaxis = np.linspace(0, 102, 102)
-
-    efield[51] = 1
 
     plt.plot(xaxis, efield, label='E')
     plt.plot(xaxis, hfield, label='H')
@@ -16,12 +23,12 @@ if __name__ == '__main__':
     plt.savefig('../images/img' + str(0))
     plt.gcf().clear()
 
-    for n in range(50):
+    for n in range(time_dim):
         new_hfield = np.zeros(102)
         new_efield = np.zeros(102)
         for i in range(1, hfield.size - 1):
             new_hfield[i] = hfield[i] - (efield[i+1] - efield[i])
-            new_efield[i] = efield[i] - (hfield[i] - hfield[i-1])
+            new_efield[i] = efield[i] - (hfield[i] - hfield[i-1]) - current[n, i]
 
             if(i == 51):
                 print(new_efield[i])
@@ -30,8 +37,9 @@ if __name__ == '__main__':
 
         plt.plot(xaxis, efield, label='E')
         plt.plot(xaxis, hfield, label='H')
+        plt.plot(xaxis, 100*current[n], label=r'C [$\times 100$]')
         plt.title(str(n+1))
         plt.legend()
         plt.ylim((-150,150))
-        plt.savefig('../images/img' + str(n+1))
+        plt.savefig('../temp/img' + str(n+1))
         plt.gcf().clear()
