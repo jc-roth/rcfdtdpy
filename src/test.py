8bdedf6c45bb63bd3ef4c2007c9c2686fdf91bc2
Jack-SLAC <jroth@dhcp-swlan-public-84-142.slac.stanford.edu>
2018-07-06 16:54:47 -0700

    Got the test.py script working, will implement these changes in the sim module in the next couple commits

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
 
-    time_dim = 50
+    time_dim = 300
     space_dim = 102
 
     hfield = np.zeros(space_dim)
@@ -11,8 +11,9 @@ if __name__ == '__main__':
 
     current = np.zeros((time_dim, space_dim))
 
-    for n in range(time_dim):
-        current[n, 49] = np.sin(2*np.pi*(n/15))
+    x = np.arange(0, time_dim+2, 1)
+
+    current[:, 49] = np.diff(np.diff(np.exp(-((x-20)**2)/(5))))
 
     xaxis = np.linspace(0, 102, 102)
 
@@ -20,26 +21,25 @@ if __name__ == '__main__':
     plt.plot(xaxis, hfield, label='H')
     plt.title(str(0))
     plt.legend()
-    plt.savefig('../images/img' + str(0))
+    plt.savefig('../temp/img' + str(0))
     plt.gcf().clear()
 
     for n in range(time_dim):
         new_hfield = np.zeros(102)
         new_efield = np.zeros(102)
         for i in range(1, hfield.size - 1):
-            new_hfield[i] = hfield[i] - (efield[i+1] - efield[i])
-            new_efield[i] = efield[i] - (hfield[i] - hfield[i-1]) - current[n, i]
+            hfield[i] = hfield[i] - (efield[i+1] - efield[i])
+            efield[i] = efield[i] - (hfield[i] - hfield[i-1]) - current[n, i]
 
-            if(i == 51):
-                print(new_efield[i])
-        hfield = new_hfield
-        efield = new_efield
+            if(i == 49):
+                print(efield[i])
+        #hfield = new_hfield
+        #efield = new_efield
 
         plt.plot(xaxis, efield, label='E')
         plt.plot(xaxis, hfield, label='H')
-        plt.plot(xaxis, 100*current[n], label=r'C [$\times 100$]')
         plt.title(str(n+1))
+        plt.ylim((-0.3, 0.3))
         plt.legend()
-        plt.ylim((-150,150))
         plt.savefig('../temp/img' + str(n+1))
         plt.gcf().clear()
