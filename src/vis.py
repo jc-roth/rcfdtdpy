79a18fdcb8fd2cfda25cf2b5f4af7c17ec7aa9b5
Jack-SLAC <jroth@dhcp-swlan-public-84-142.slac.stanford.edu>
2018-07-09 15:26:54 -0700

    Fixed bugs with unit setting in the vis module and added unit setting to the contor function

--- src/vis.py
+++ src/vis.py
@@ -12,26 +12,26 @@ from matplotlib import animation
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
 
-def plot(sim, n, fname=None, xscale=1, yscale_e=1, yscale_h=1, xunit='NA', yunit_e='NA', yunit_h='NA'):
+def plot(sim, n, fname=None, iscale=1, escale=1, hscale=1, iunit='NA', eunit='NA', hunit='NA'):
     """
     Displays the E and H-fields at a given index in time.
 
     :param sim: The simulation to visualize
     :param n: The temporal index :math:`n` to display
     :param fname: The filename to export to, does not export if blank
-    :param xscale: The scalar factor that the x-axis is scaled by
-    :param yscale_e: The scalar factor that the y-axis of the E-field is scaled by
-    :param yscale_h: The scalar factor that the y-axis of the H-field is scaled by
-    :param xunit: The units given to the x-label
-    :param yunit_e: The units given to the y-label for the E-field axis
-    :param yunit_h: The units given to the y-label for the H-field axis
+    :param iscale: The scalar factor that the x-axis is scaled by
+    :param escale: The scalar factor that the y-axis of the E-field is scaled by
+    :param hscale: The scalar factor that the y-axis of the H-field is scaled by
+    :param iunit: The units given to the x-label
+    :param eunit: The units given to the y-label for the E-field axis
+    :param hunit: The units given to the y-label for the H-field axis
     """
     # Export variables from the simulation
     n_arr, i, e, h, c = sim.export()
     # Apply scale factors
-    i *= xscale
-    e *= yscale_e
-    h *= yscale_h
+    i *= iscale
+    e *= escale
+    h *= hscale
     # Create a new figure and set x-limits
     fig = plt.figure()
     ax0 = plt.axes()
@@ -46,13 +46,13 @@ def plot(sim, n, fname=None, xscale=1, yscale_e=1, yscale_h=1, xunit='NA', yunit
     ax1.set_ylim(hmax, -hmax)
     # Plot
     le = ax0.plot(i, e[n], color='#1f77b4')
-    lh = ax1.plot(i, h[n], color='#ff7f0e')
+    lh = ax1.plot(i+np.diff(i[0:2]), h[n], color='#ff7f0e') # Note the np.diff() function is used to offset the H-field plot as required by the Yee cell
     # Add legend
     ax0.legend((le + lh), ('E', 'H'), loc=1)
     # Label axes
-    ax0.set_xlabel('$z$ [%s]' % xunit)
-    ax0.set_ylabel('$E$ [%s]' % yunit_e)
-    ax1.set_ylabel('$H$ [%s]' % yunit_h)
+    ax0.set_xlabel('$z$ [%s]' % iunit)
+    ax0.set_ylabel('$E$ [%s]' % eunit)
+    ax1.set_ylabel('$H$ [%s]' % hunit)
     # Final preparations
     plt.tight_layout()
     # Display or save
@@ -61,20 +61,26 @@ def plot(sim, n, fname=None, xscale=1, yscale_e=1, yscale_h=1, xunit='NA', yunit
     else:
         plt.savefig(fname)
 
-def timeseries(sim, fname=None, interval=10, xscale=1, yscale_e=1, yscale_h=1, xunit='NA', yunit_e='NA', yunit_h='NA'):
+def timeseries(sim, fname=None, interval=10, iscale=1, escale=1, hscale=1, iunit='NA', eunit='NA', hunit='NA'):
     """
     Animates the E and H-fields in time.
 
     :param sim: The simulation to visualize
     :param interval: The interval between timesteps in milliseconds
     :param fname: The filename to export to, does not export if blank
+    :param iscale: The scalar factor that the x-axis is scaled by
+    :param escale: The scalar factor that the y-axis of the E-field is scaled by
+    :param hscale: The scalar factor that the y-axis of the H-field is scaled by
+    :param iunit: The units given to the x-label
+    :param eunit: The units given to the y-label for the E-field axis
+    :param hunit: The units given to the y-label for the H-field axis
     """
     # Export variables from the simulation
     n, i, e, h, c = sim.export()
     # Apply scale factors
-    i *= xscale
-    e *= yscale_e
-    h *= yscale_h
+    i *= iscale
+    e *= escale
+    h *= hscale
     # Create a new figure and set x-limits
     fig = plt.figure()
     ax0 = plt.axes()
@@ -93,9 +99,9 @@ def timeseries(sim, fname=None, interval=10, xscale=1, yscale_e=1, yscale_h=1, x
     # Add legend
     ax0.legend((le, lh), ('E', 'H'), loc=1)
     # Label axes
-    ax0.set_xlabel('$z$ [%s]' % xunit)
-    ax0.set_ylabel('$E$ [%s]' % yunit_e)
-    ax1.set_ylabel('$H$ [%s]' % yunit_h)
+    ax0.set_xlabel('$z$ [%s]' % iunit)
+    ax0.set_ylabel('$E$ [%s]' % eunit)
+    ax1.set_ylabel('$H$ [%s]' % hunit)
     # Final preparations
     plt.tight_layout()
     # Define the initialization and update functions
@@ -105,7 +111,7 @@ def timeseries(sim, fname=None, interval=10, xscale=1, yscale_e=1, yscale_h=1, x
         return (le,lh)
     def update(n):
         le.set_data(i, e[n])
-        lh.set_data(i, h[n])
+        lh.set_data(i+np.diff(i[0:2]), h[n]) # Note the np.diff() function is used to offset the H-field plot as required by the Yee cell
         return (le,lh)
     # Run animation
     anim = animation.FuncAnimation(fig, update, frames=len(n), interval=interval, init_func=init, blit=True)
@@ -115,17 +121,28 @@ def timeseries(sim, fname=None, interval=10, xscale=1, yscale_e=1, yscale_h=1, x
     else:
         anim.save(fname, fps=int(1000/interval))
 
-def contor(sim, fname=None, nlevels=500):
+def contor(sim, fname=None, nlevels=500, nscale=1, iscale=1, escale=1, hscale=1, nunit='NA', iunit='NA', eunit='NA', hunit='NA'):
     """
     Displays the E and H-fields on a contor plot
 
     :param sim: The simulation to visualize
     :param fname: The filename to export to, does not export if blank
     :param nlevels: The number of color levels to display the field intensities with
+    :param iscale: The scalar factor that the x-axis is scaled by
+    :param escale: The scalar factor that the y-axis of the E-field is scaled by
+    :param hscale: The scalar factor that the y-axis of the H-field is scaled by
+    :param iunit: The units given to the x-label
+    :param eunit: The units given to the y-label for the E-field axis
+    :param hunit: The units given to the y-label for the H-field axis
     """
     # Extract simulation results and parameters
     n, i, e, h, c = sim.export()
     n0, n1, dn, i0, i1, di = sim.get_bound_res()
+    # Apply scale factors
+    i *= iscale
+    n *= nscale
+    e *= escale
+    h *= hscale
     # Generate mesh grid
     ngrid, igrid = np.mgrid[n0:n1:dn, i0:i1:di]
     # Create figure and axes
@@ -143,12 +160,12 @@ def contor(sim, fname=None, nlevels=500):
     # Plot the E-field
     emap = axes[0].pcolormesh(igrid, ngrid, e, cmap=cmap, norm=norm)
     fig.colorbar(emap, ax=axes[0])
-    axes[0].set_ylabel('E\ntime [n]')
+    axes[0].set_ylabel('$E$ - $t$ [%s]' % nunit)
     # Plot the H-field
     hmap = axes[1].pcolormesh(igrid, ngrid, h, cmap=cmap, norm=norm)
     fig.colorbar(hmap, ax=axes[1])
-    axes[1].set_xlabel('space [i]')
-    axes[1].set_ylabel('H\ntime [n]')
+    axes[1].set_xlabel('$z$ [%s]' % iunit)
+    axes[1].set_ylabel('$H$ - $t$ [%s]' % nunit)
     # Display or save
     if fname is None:
         plt.show()
