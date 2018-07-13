e563ce5a89979da493255e8cec4232d297da7816
Jack-SLAC <jroth@dhcp-swlan-public-84-142.slac.stanford.edu>
2018-07-12 17:08:52 -0700

    Updated __init__.py file

--- rcfdtd_sim/rcfdtd_sim/__init__.py
+++ rcfdtd_sim/rcfdtd_sim/__init__.py
@@ -1,2 +1,3 @@
-from .sim import *
-from .vis import *
+from .sim import Sim, Mat
+from .vis import plot, plot_loc, timeseries, contor
+__all__ = ['Sim', 'Mat', 'plot', 'plot_loc', 'timeseries', 'contor']
