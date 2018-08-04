fixed bug in the calculation of self._chi0_1

rcfdtd_sim/rcfdtd_sim/sim.py
@@ -488,8 +488,8 @@ class Mat:
             for mi in range(self._matlen):
                 if np.abs(b_min_g[j, mi]) < 1e-5:
                     # beta-gamma is small, avoid divide by zero error
-                    self._chi0_1[j, mi] = np.complex64(0)
-                    self._chi0_2[j, mi] = np.multiply(np.divide(self._mata2[j, mi], -min_b_min_g[j, mi]), np.subtract(self._exp_2[j, mi], 1)) # TODO: I don't entirely understand the purpose of the added negative in this computation. Follow up with Ben.
+                    self._chi0_1[j, mi] = -self._mata1[j, mi]*self._dn
+                    self._chi0_2[j, mi] = np.multiply(np.divide(self._mata2[j, mi], min_b_min_g[j, mi]), np.subtract(self._exp_2[j, mi], 1))
                 else:
                     # beta-gamma is not small, calculate normally
                     self._chi0_1[j, mi] = np.multiply(np.divide(self._mata1[j, mi], b_min_g[j, mi]), np.subtract(self._exp_1[j, mi], 1))